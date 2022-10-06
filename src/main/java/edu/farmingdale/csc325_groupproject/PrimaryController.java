2ed5fe5bcd542ceabea1c2ad19773aa3370f9b1f
quint <quint@LAPTOP-GRARRPGK>
2022-10-06 00:13:44 -0400

    Added Id's to the implemented fields

--- src/main/java/edu/farmingdale/csc325_groupproject/PrimaryController.java
+++ src/main/java/edu/farmingdale/csc325_groupproject/PrimaryController.java
@@ -1,15 +1,35 @@
 package edu.farmingdale.csc325_groupproject;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class PrimaryController {
-    @FXML
-    private Button primaryButton;
+public class PrimaryController implements Initializable{
     
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("Menu");
+    private Button loginButton;
+
+    @FXML
+    private ImageView logoView;
+
+    @FXML
+    private TextField userInput;
+
+    @FXML
+    private TextField userPassword;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //Image img = new Image("./OIP.jpg");
+        //logoView.setImage(img);
     }
+    
+   
 }
