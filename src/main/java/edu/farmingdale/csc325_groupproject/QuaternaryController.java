b36f88f02ee54956ae57c4cc1e262a44f9a9aacc
jonat <jonat@DESKTOP-RPROJST.mynetworksettings.com>
2022-10-07 22:18:59 -0400

    Implemented Certain functionality to Complaints Window

--- src/main/java/edu/farmingdale/csc325_groupproject/QuaternaryController.java
+++ src/main/java/edu/farmingdale/csc325_groupproject/QuaternaryController.java
@@ -2,13 +2,52 @@ package edu.farmingdale.csc325_groupproject;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 public class QuaternaryController implements Initializable {
+    
+    @FXML
+    private TextField codeTxt;
 
+    @FXML
+    private TextField dateTxt;
+
+    @FXML
+    private TextField descTxt;
+
+    @FXML
+    private TextField distTxt;
+
+    @FXML
+    private TextField loc1Txt;
+
+    @FXML
+    private TextField locTxt;
+
+    @FXML
+    private TextField nameTxt;
+
+    @FXML
+    private TextField neighTxt;
+
+    @FXML
+    private TextField postTxt;
+
+    @FXML
+    private TextField timeTxt;
+
+    @FXML
+    private TextField totTxt;
+
+    @FXML
+    private TextField weapTxt;
+    
+    private ArrayList<Complaints> comps = new ArrayList<Complaints>();
     /**
      * Initializes the controller class.
      */
@@ -19,8 +58,23 @@ public class QuaternaryController implements Initializable {
     
     @FXML
     void InputData(ActionEvent event) {
-
+        Complaints b = new Complaints();
+        b.CrimeDate = dateTxt.getText();
+        b.CrimeTime = timeTxt.getText();
+        b.CrimeCode = codeTxt.getText();
+        b.CrimeLoc = locTxt.getText();
+        b.CrimeDesc = descTxt.getText();
+        b.CrimeWeap = weapTxt.getText();
+        b.CrimePost = Integer.parseInt(postTxt.getText());
+        b.CrimeDist = distTxt.getText();
+        b.CrimeNeigh = neighTxt.getText();
+        b.CrimeLoc1 = loc1Txt.getText();
+        b.CrimeTot = Integer.parseInt(totTxt.getText());
+        b.CrimeName = nameTxt.getText();
+        comps.add(b);
     }
+    
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("Menu");
