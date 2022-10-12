5a7d5119307a0d32e5bed2ff0fbfe79266f7bea3
jonat <jonat@DESKTOP-RPROJST.mynetworksettings.com>
2022-10-11 21:21:13 -0400

    Added JDBC connection and capability to insert into DB

--- src/main/java/edu/farmingdale/csc325_groupproject/QuaternaryController.java
+++ src/main/java/edu/farmingdale/csc325_groupproject/QuaternaryController.java
@@ -2,7 +2,13 @@ package edu.farmingdale.csc325_groupproject;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.*;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
@@ -44,6 +50,9 @@ public class QuaternaryController implements Initializable {
     } 
     @FXML
     void InputData(ActionEvent event) {
+        
+        
+
         Complaint b = new Complaint();
         b.CrimeDate = dateTxt.getText();
         b.CrimeTime = timeTxt.getText();
@@ -58,6 +67,37 @@ public class QuaternaryController implements Initializable {
         b.CrimeTot = Integer.parseInt(totTxt.getText());
         b.CrimeName = nameTxt.getText();
         comps.add(b);
+        
+        String databaseURL = "";
+        Connection conn = null;
+        try {
+            databaseURL = "jdbc:ucanaccess://.//Crime Management.accdb";
+            conn = DriverManager.getConnection(databaseURL);
+            
+            String sql = "INSERT INTO Complaint (CrimeDate, CrimeTime, CrimeCode, Location, Description, Weapon, Post, District, Neighborhood, "
+                    + "Location 1, Total Incidents, Full Name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, b.getCrimeDate());
+            preparedStatement.setString(2, b.getCrimeTime());
+            preparedStatement.setString(3, b.getCrimeCode());
+            preparedStatement.setString(4, b.getCrimeLoc());
+            preparedStatement.setString(5, b.getCrimeDesc());
+            preparedStatement.setString(6, b.getCrimeWeap());
+            preparedStatement.setInt(7, b.getCrimePost());
+            preparedStatement.setString(8, b.getCrimeDist());
+            preparedStatement.setString(9, b.getCrimeNeigh());
+            preparedStatement.setString(10, b.getCrimeLoc1());
+            preparedStatement.setInt(11, b.getCrimeTot());
+            preparedStatement.setString(12, b.getCrimeName());
+            int row = preparedStatement.executeUpdate();
+            if (row > 0) {
+                System.out.println("Row inserted");
+            }
+    
+        } catch (SQLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
     @FXML
