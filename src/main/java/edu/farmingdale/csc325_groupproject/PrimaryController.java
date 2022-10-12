50d71a4bbc9372c59430f7e38c5e4ba4c531babc
quint <quint@LAPTOP-GRARRPGK>
2022-10-11 23:36:35 -0400

    Implemented reading credentials and comparing to database

--- src/main/java/edu/farmingdale/csc325_groupproject/PrimaryController.java
+++ src/main/java/edu/farmingdale/csc325_groupproject/PrimaryController.java
@@ -2,6 +2,11 @@ package edu.farmingdale.csc325_groupproject;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,14 +28,45 @@ public class PrimaryController implements Initializable{
     
     @FXML
     private void verifyCredentials(ActionEvent event) throws IOException {
-        //TODO: Switch scenes between the Authentication screen and the Menu screen
-        //TODO: Read the person credentials with the Admin class
-        //TODO: Correlate with the person that has the database to search for credentials in database 
-        App.setRoot("Menu");   
+
+
+        String username = userInput.getText();
+        String password = userPassword.getText();
+        
+        Connection conn;
+        String databaseURL;
+         try {
+            
+            databaseURL = "jdbc:ucanaccess://.//Crime Management.accdb";
+            conn = DriverManager.getConnection(databaseURL);
+            String tableName = "Users";
+            Statement stmt = conn.createStatement();
+            ResultSet result =  stmt.executeQuery("select * from " + tableName);
+            
+            while(result.next()){
+                String user = result.getString("username");
+                String pw = result.getString("password");
+                if (user.equals(username) && pw.equals(password)){
+                    App.setRoot("Menu");   
+                }else {
+                    System.out.println("invalid user and password");
+                }    
+                
+            
+                //System.out.printf("%s %s", user, pw);  
+            }
+         }
+         
+         catch(SQLException e){
+             
+         }    
     }
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Image img = new Image("/pics/OIP.jpg");
-        logoView.setImage(img);
+        logoView.setImage(img);           
     }
+    
 }
--- src/main/java/edu/farmingdale/csc325_groupproject/User.java
+++ src/main/java/edu/farmingdale/csc325_groupproject/User.java
@@ -4,4 +4,40 @@ public class User
 {
     private String username;
     private String password;
+    private int securityLvl;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSecurityLevel() {
+        return securityLvl;
+    }
+
+    public void setSecurityLevel(int securityLevel) {
+        this.securityLvl = securityLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "username=" + username + ", password=" + password + ", securityLevel=" + securityLvl + '}';
+    }
+    
+    
+    
 }
