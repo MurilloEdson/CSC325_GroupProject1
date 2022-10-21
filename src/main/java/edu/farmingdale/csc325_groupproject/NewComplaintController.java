cbe8977b769d26bb3714cb8ddbcc85d255a42e9f
MurilloEdson <>
2022-10-21 19:37:42 -0400

    Switches made and information transferred

--- src/main/java/edu/farmingdale/csc325_groupproject/NewComplaintController.java
+++ src/main/java/edu/farmingdale/csc325_groupproject/NewComplaintController.java
@@ -7,120 +7,35 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.*;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class NewComplaintController implements Initializable {
-    
-    @FXML
-    private TextField codeTxt;
-    @FXML
-    private TextField dateTxt;
-    @FXML
-    private TextField descTxt;
-    @FXML
-    private TextField distTxt;
-    @FXML
-    private TextField loc1Txt;
-    @FXML
-    private TextField locTxt;
-    @FXML
-    private TextField nameTxt;
-    @FXML
-    private TextField neighTxt;
-    @FXML
-    private TextField postTxt;
-    @FXML
-    private TextField timeTxt;
-    @FXML
-    private TextField totTxt;
-    @FXML
-    private TextField weapTxt;
-    
-    @FXML
-    private ImageView logoView;
-    
-    @FXML
-    private ImageView logoViewHelp;
-    
+
     private ArrayList<Complaint> comps = new ArrayList<Complaint>();
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Image img = new Image("/pics/OIP.jpg");
-        logoView.setImage(img);
-        Image img1 = new Image("/pics/helpIMG.png");
-        logoViewHelp.setImage(img1);
+        
     } 
     
     @FXML
     void helpWindow(MouseEvent event) {
-        System.out.println("pressed");
-                
+        /*System.out.println("pressed");
+        
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Help:");
         alert.setContentText("Please Input the data of your complaint where you can, \n it is okay to leave fields blank.");
-        alert.show();
+        alert.show();*/
     }
     
     @FXML
     void InputData(ActionEvent event) {
-        
-        
-
-        Complaint b = new Complaint();
-        b.CrimeDate = dateTxt.getText();
-        b.CrimeTime = timeTxt.getText();
-        b.CrimeCode = codeTxt.getText();
-        b.CrimeLoc = locTxt.getText();
-        b.CrimeDesc = descTxt.getText();
-        b.CrimeWeap = weapTxt.getText();
-        b.CrimePost = Integer.parseInt(postTxt.getText());
-        b.CrimeDist = distTxt.getText();
-        b.Neighborhood = neighTxt.getText();
-        b.CrimeLoc1 = loc1Txt.getText();
-        b.CrimeTot = Integer.parseInt(totTxt.getText());
-        b.CrimeName = nameTxt.getText();
-        comps.add(b);
-        
-        String databaseURL = "";
-        Connection conn = null;
-        try {
-            databaseURL = "jdbc:ucanaccess://.//Crime Management.accdb";
-            conn = DriverManager.getConnection(databaseURL);
-            
-            String sql = "INSERT INTO Complaint (CrimeDate, CrimeTime, CrimeCode, Location, Description, Weapon, Post, District, Neighborhood, "
-                    + "Location 1, Total Incidents, Full Name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            preparedStatement.setString(1, b.getCrimeDate());
-            preparedStatement.setString(2, b.getCrimeTime());
-            preparedStatement.setString(3, b.getCrimeCode());
-            preparedStatement.setString(4, b.getCrimeLoc());
-            preparedStatement.setString(5, b.getCrimeDesc());
-            preparedStatement.setString(6, b.getCrimeWeap());
-            preparedStatement.setInt(7, b.getCrimePost());
-            preparedStatement.setString(8, b.getCrimeDist());
-            preparedStatement.setString(9, b.getNeighborhood());
-            preparedStatement.setString(10, b.getCrimeLoc1());
-            preparedStatement.setInt(11, b.getCrimeTot());
-            preparedStatement.setString(12, b.getCrimeName());
-            int row = preparedStatement.executeUpdate();
-            if (row > 0) {
-                System.out.println("Row inserted");
-            }
-    
-        } catch (SQLException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        }
         
     }
     
