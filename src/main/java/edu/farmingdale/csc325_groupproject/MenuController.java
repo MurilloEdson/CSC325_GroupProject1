3c31bfcbec4893f89133713853f6255f40e06331
user <user@192.168.1.162>
2022-10-21 23:59:02 -0400

    Added logo to menu window

--- src/main/java/edu/farmingdale/csc325_groupproject/MenuController.java
+++ src/main/java/edu/farmingdale/csc325_groupproject/MenuController.java
@@ -1,14 +1,21 @@
 package edu.farmingdale.csc325_groupproject;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class MenuController {
+public class MenuController implements Initializable {
     
     @FXML
     private void logout() throws IOException {
         App.setRoot("SignIn");
     }
+    @FXML
+    private ImageView menuLogo;
     
     @FXML
     private void switchToMainDisplay() throws IOException{
@@ -18,5 +25,10 @@ public class MenuController {
     @FXML
     private void switchToNewComplaint() throws IOException{
         App.setRoot("NewComplaint");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Image img = new Image("/pics/OIP.jpg");
+        menuLogo.setImage(img);
     }
 }
